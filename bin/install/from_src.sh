159532849a35b9087c6ad33f8a1507428f5ed728
Jorge Langa <jorge.langa.arranz@gmail.com>
2017-04-11 11:58:14 +0200

    Bugfix

--- bin/install/from_src.sh
+++ bin/install/from_src.sh
@@ -8,7 +8,7 @@ pushd src/
 git clone https://github.com/jlanga/exon_finder.git .
 pushd exon_finder/
 python setup.py test
-pip3 install --user --editable .
+pip install --user --editable .
 popd
 
 popd
