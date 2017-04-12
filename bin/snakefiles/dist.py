87f7880287a5780616e1de44cc7c473d1e713237
Jorge Langa <jorge.langa.arranz@gmail.com>
2017-04-12 10:35:24 +0200

    Bugfix
    - dist.py: wasn't asking for exome.fa.fai

--- bin/snakefiles/dist.py
+++ bin/snakefiles/dist.py
@@ -2,7 +2,7 @@ rule dist_plot:
     input:
         raw_fai = exons + "raw.fa.fai",
         filtered_fai = exons + "filtered.fa.fai",
-        true_fai = config["reference"]["exome"]
+        true_fai = config["reference"]["exome"] + ".fai"
     output:
         dist + "exon_histogram.pdf",
         dist + "exon_density.pdf"
@@ -11,4 +11,4 @@ rule dist_plot:
     benchmark:
         dist + "plot.json"
     shell:
-        "Rscript bin/plot_exon_length_distribution.R"
+        "Rscript bin/plot_exon_length_distribution.R 2> {log}"
