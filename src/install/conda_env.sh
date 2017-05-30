a4060b1eb90ead58de247f9053746d761ec19f1b
Jorge Langa <jorge.langa.arranz@gmail.com>
2017-05-30 16:47:53 +0200

    Bugfix when there is a cache

--- src/install/conda_env.sh
+++ src/install/conda_env.sh
@@ -2,4 +2,7 @@ conda config --add channels conda-forge
 conda config --add channels defaults
 conda config --add channels r
 conda config --add channels bioconda
-conda env create --name exfi_validation --file requirements.txt
+if test -e $HOME/miniconda3/envs/exfi_validation ; then
+else
+    conda env create --name exfi_validation --file requirements.txt
+fi
