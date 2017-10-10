87ea7f5e2bd448a39898b09feed87a28340d958a
Jorge Langa <jorge.langa.arranz@gmail.com>
2017-10-10 17:18:03 +0200

    Updated installers

--- src/install/from_src.sh
+++ src/install/from_src.sh
@@ -14,13 +14,28 @@ popd
 
 
 
-# biobloomtools via github
-git clone https://github.com/bcgsc/biobloom biobloom
-pushd biobloom/
-./autogen.sh
-./configure
-make -j
-cp \
-    BioBloomMaker/biobloommaker \
-    BioBloomCategorizer/biobloomcategorizer ../../bin/
+pushd /opt/
+
+# SDSL-lite
+# https://hub.docker.com/r/adamnovak/sequence-graphs/~/dockerfile/
+if [[ ! -d sdsl-lite/ ]]; then
+    git clone https://github.com/simongog/sdsl-lite.git
+fi
+pushd sdsl-lite/ && \
+sudo ./install.sh /usr/local/ && \
+popd
+
+# biobloomtools
+# as in https://github.com/bcgsc/biobloom
+if [[ ! -d biobloom/ ]]; then
+    git clone https://github.com/bcgsc/biobloom.git
+fi
+pushd biobloom/ && \
+git submodule update --init && \
+./autogen.sh && \
+./configure --prefix=/usr/local/ && \
+make -j 2 && \
+sudo make install && \
+popd
+
 popd
