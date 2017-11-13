e230efdc48aad661ea8876b32a721c4a2f78eabe
Jorge Langa <jorge.langa.arranz@gmail.com>
2017-11-14 00:03:43 +0100

    Using conda env yaml files

--- src/install/conda_env.sh
+++ src/install/conda_env.sh
@@ -1,11 +1,6 @@
-conda config --add channels conda-forge
-conda config --add channels defaults
-conda config --add channels r
-conda config --add channels bioconda
-
-if [ ! -e $HOME/miniconda3/envs/exfi_validation ] ; then
-    conda env create --name exfi_validation --file requirements.txt
+if [ ! -e $(which conda)/.miniconda3/envs/exfi_validation ] ; then
+    conda env create --name exfi_validation --file environment.yml
 else
     source activate exfi_validation
-    conda install --yes --file requirements.txt
+    conda env update --file environment.yml
 fi
