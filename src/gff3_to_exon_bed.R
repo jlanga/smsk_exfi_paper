f7192d824b5e0f21e950bdca03ddd74482c8319a
Jorge Langa <jorge.langa.arranz@gmail.com>
2017-11-14 23:36:42 +0100

    Read irregular gff files: Ensembl + NCBI Genomes

--- src/gff3_to_exon_bed.R
+++ src/gff3_to_exon_bed.R
@@ -4,43 +4,62 @@ args <- commandArgs(trailingOnly=TRUE)
 
 infile <- args[1]
 outfile <- args[2]
-gff3_to_bed <- function(path){
-    
-    path %>%
+
+gff_to_bed <- function(path){
+
+    gff_columns <- c("seqid", "source", "type", "start", "end", "score", "strand", "phase", "attribute")
+
+
+    raw <- path1 %>%
         read_tsv(
-            col_names = c("seqid", "source", "type", "start", "end", "score", "strand", "phase", "attribute"),
-            col_types = c("cccddcccc"),
+            col_names = gff_columns,
+            col_types = c("ccciicccc"),
             comment = "#",
-            progress = FALSE,
+            progress = TRUE,
             na = "."
         ) %>%
-        select(-c(source, score, phase)) %>%
+        select(-c(seqid, source, score, phase)) %>%
         filter(type == "exon") %>%
         select(-type) %>%
         extract(
             col = attribute,
-            into = c("transcript_id", "exon_id", "rank"),
-            regex = "Parent=transcript:(.+);Name=.*;exon_id=(.+);rank=(.+);version=.+",
+            into = c("field_type", "transcript_id"),
+            regex = '(transcript_id=|Parent=transcript:)([A-Za-z0-9_.]+)',
             remove=TRUE,
             convert=TRUE
         ) %>%
-        arrange(transcript_id, rank) %>%
-        mutate(length = end - start + 1) %>% 
+        select(-field_type)
+
+    positive <- raw %>%
+        filter(strand == "+") %>%
+        select(-strand) %>%
         group_by(transcript_id) %>%
-        mutate(
-            transcript_end = cumsum(length) %>% as.integer(),
-            transcript_start = transcript_end - length %>% as.integer(),
-        ) %>%
-        select(
-            chrom = transcript_id,
-            chromStart = transcript_start,
-            chromEnd = transcript_end,
-            name = exon_id
-        )
+        arrange(transcript_id, start, end) %>%
+        ungroup()
+
+    negative <- raw %>%
+        filter(strand == "-") %>%
+        select(-strand) %>%
+        group_by(transcript_id) %>%
+        arrange(transcript_id, desc(start), desc(end)) %>%
+        ungroup()
+
+    rbind(positive, negative) %>%
+    mutate(length = end - start + 1) %>%
+    group_by(transcript_id) %>%
+    mutate(
+        transcript_end = cumsum(length),
+        transcript_start = transcript_end - length
+    ) %>%
+    select(
+        chrom = transcript_id,
+        chromStart = transcript_start,
+        chromEnd = transcript_end
+    ) %>%
+    ungroup()
 }
 
 infile %>%
     gff3_to_bed() %>%
     format_tsv(col_names=FALSE) %>%
     cat()
-
