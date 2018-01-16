263adc7b6d8737d190458bd6f530e94aeef19b47
Jorge Langa <jorge.langa.arranz@gmail.com>
2018-01-16 16:24:15 +0100

    Updated exfi v1.4.2

--- src/snakefiles/exfi.py
+++ src/snakefiles/exfi.py
@@ -29,9 +29,9 @@ rule exfi_build_baited_bloom_filter:
         "build_baited_bloom_filter "
             "--input-fasta {input.transcriptome} "
             "--kmer {params.kmer} "
-            "--bloom-size={params.size} "
-            "--threads={threads} "
-            "--levels={params.levels} "
+            "--bloom-size {params.size} "
+            "--threads {threads} "
+            "--levels {params.levels} "
             "--output-bloom {output.bloom_filter} "
             "{input.fq_gz} "
         "2> {log}"
@@ -52,7 +52,9 @@ rule exfi_build_splice_graph:
         gfa = exfi + "splice_graph.gfa"
     params:
         kmer = config["exfi"]["kmer"],
-        max_fp_bases = config["exfi"]["max_fp_bases"]
+        max_fp_bases = config["exfi"]["max_fp_bases"],
+        max_overlap = config["exfi"]["max_overlap"],
+        max_gap_size = config["exfi"]["max_gap_size"]
     threads:
         1
     log:
@@ -60,29 +62,31 @@ rule exfi_build_splice_graph:
     benchmark:
         exfi + "build_splice_graph.json"
     shell:
-        "build_splicegraph "
+        "build_splice_graph "
             "--input-fasta {input.transcriptome} "
             "--input-bloom {input.bloom_filter} "
             "--kmer {params.kmer} "
             "--max-fp-bases {params.max_fp_bases} "
+            "--max-overlap {params.max_overlap} "
+            "--max-gap-size {params.max_gap_size} "
             "--output-gfa {output.gfa} "
         "2> {log}"
 
 
 
-rule exfi_gfa_to_exons:
+rule exfi_gfa1_to_exons:
     input:
         gfa = exfi + "splice_graph.gfa"
     output:
         exons = exfi + "exons.fa"
     params:
-        extra = config["exfi"]["gfa_to_exons_extra"]
+        extra = config["exfi"]["gfa1_to_exons_extra"]
     log:
-        exfi + "gfa_to_exons.log"
+        exfi + "gfa1_to_exons.log"
     benchmark:
-        exfi + "gfa_to_exons.json"
+        exfi + "gfa1_to_exons.json"
     shell:
-        "gfa_to_exons "
+        "gfa1_to_exons "
             "--input-gfa {input.gfa} "
             "--output-fasta {output.exons} "
             "{params.extra} "
@@ -90,19 +94,19 @@ rule exfi_gfa_to_exons:
 
 
 
-rule exfi_gfa_to_gapped_transcript:
+rule exfi_gfa1_to_gapped_transcript:
     input:
         gfa = exfi + "splice_graph.gfa"
     output:
         transcripts = exfi + "gapped_transcripts.fa"
     params:
-        extra = config["exfi"]["gfa_to_gapped_transcript_extra"]
+        extra = config["exfi"]["gfa1_to_gapped_transcript_extra"]
     log:
-        exfi + "gfa_to_gapped_transcripts.log"
+        exfi + "gfa1_to_gapped_transcripts.log"
     benchmark:
-        exfi + "gfa_to_gapped_transcripts.json"
+        exfi + "gfa1_to_gapped_transcripts.json"
     shell:
-        "gfa_to_gapped_transcripts "
+        "gfa1_to_gapped_transcripts "
             "--input-gfa {input.gfa} "
             "--output-fasta {output.transcripts} "
             "{params.extra} "
