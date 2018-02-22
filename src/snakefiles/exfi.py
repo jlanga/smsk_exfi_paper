e943c520ac90a42eddcb9ff736070b7708c87c9f
Jorge Langa <jorge.langa.arranz@gmail.com>
2018-02-22 17:39:29 +0100

    --use-conda

--- src/snakefiles/exfi.py
+++ src/snakefiles/exfi.py
@@ -25,13 +25,14 @@ rule exfi_build_baited_bloom_filter:
         exfi + "build_baited_bloom_filter.log"
     benchmark:
         exfi + "build_baited_bloom_filter.json"
+    conda: "exfi.yml"
     shell:
         "build_baited_bloom_filter "
             "--input-fasta {input.transcriptome} "
             "--kmer {params.kmer} "
-            "--bloom-size {params.size} "
-            "--threads {threads} "
-            "--levels {params.levels} "
+            "--bloom-size={params.size} "
+            "--threads={threads} "
+            "--levels={params.levels} "
             "--output-bloom {output.bloom_filter} "
             "{input.fq_gz} "
         "2> {log}"
@@ -52,30 +53,23 @@ rule exfi_build_splice_graph:
         gfa = exfi + "splice_graph.gfa"
     params:
         kmer = config["exfi"]["kmer"],
-        max_fp_bases = config["exfi"]["max_fp_bases"],
-        max_overlap = config["exfi"]["max_overlap"],
-        max_gap_size = config["exfi"]["max_gap_size"]
-    threads:
-        1
-    log:
-        exfi + "build_splice_graph.log"
-    benchmark:
-        exfi + "build_splice_graph.json"
+        max_fp_bases = config["exfi"]["max_fp_bases"]
+    threads: 1
+    log: exfi + "build_splice_graph.log"
+    benchmark: exfi + "build_splice_graph.json"
+    conda: "exfi.yml"
     shell:
         "build_splice_graph "
             "--input-fasta {input.transcriptome} "
             "--input-bloom {input.bloom_filter} "
-            "--correct "
             "--kmer {params.kmer} "
             "--max-fp-bases {params.max_fp_bases} "
-            "--max-overlap {params.max_overlap} "
-            "--max-gap-size {params.max_gap_size} "
             "--output-gfa {output.gfa} "
         "2> {log}"
 
 
 
-rule exfi_gfa1_to_exons:
+rule exfi_gfa_to_exons:
     input:
         gfa = exfi + "splice_graph.gfa"
     output:
@@ -83,9 +77,10 @@ rule exfi_gfa1_to_exons:
     params:
         extra = config["exfi"]["gfa1_to_exons_extra"]
     log:
-        exfi + "gfa1_to_exons.log"
+        exfi + "gfa_to_exons.log"
     benchmark:
-        exfi + "gfa1_to_exons.json"
+        exfi + "gfa_to_exons.json"
+    conda: "exfi.yml"
     shell:
         "gfa1_to_exons "
             "--input-gfa {input.gfa} "
@@ -95,7 +90,7 @@ rule exfi_gfa1_to_exons:
 
 
 
-rule exfi_gfa1_to_gapped_transcript:
+rule exfi_gfa_to_gapped_transcript:
     input:
         gfa = exfi + "splice_graph.gfa"
     output:
@@ -103,9 +98,10 @@ rule exfi_gfa1_to_gapped_transcript:
     params:
         extra = config["exfi"]["gfa1_to_gapped_transcript_extra"]
     log:
-        exfi + "gfa1_to_gapped_transcripts.log"
+        exfi + "gfa_to_gapped_transcripts.log"
     benchmark:
-        exfi + "gfa1_to_gapped_transcripts.json"
+        exfi + "gfa_to_gapped_transcripts.json"
+    conda: "exfi.yml"
     shell:
         "gfa1_to_gapped_transcripts "
             "--input-gfa {input.gfa} "
